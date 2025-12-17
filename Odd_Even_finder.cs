8c0b29871f433e108aaf45fc9b52598724492a7b
S-kaif123 <kaifbhombal.kb@gmail.com>
2025-12-17 09:26:41 +0530

    Add project files.

---
+++ Odd_Even_finder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Odd_Even_finder
+{
+	 
+
+	internal void Odd_Even_finding_method(int num)
+	{
+		if (num % 2 == 0) {
+			Console.WriteLine("The number " + num + "is Even");
+		}
+		else {
+			Console.WriteLine("The number " + num + "is Odd");
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Odd_Even_finder obj = new Odd_Even_finder();
+        Console.WriteLine("Enter your Nuumber ");
+        Int32 num=Convert.ToInt32( Console.ReadLine());
+
+        obj.Odd_Even_finding_method(num);
+
+    }
+}
